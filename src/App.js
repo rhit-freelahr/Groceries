5dee23e4e7dd9704f82533cedcf4b517fb4389d7
freelahr <freelahr@rose-hulman.edu>
2024-02-14 18:23:02 -0500

    made modal

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import '@radix-ui/themes/styles.css';
 import { Container, Theme } from '@radix-ui/themes';
 import { ScrollArea, Separator, Grid, Checkbox, Table, Flex, Box, Heading, Inset, Text, Button, Card, Strong } from '@radix-ui/themes';
+import * as Dialog from "@radix-ui/react-dialog"
 import jsonData from './data.json'
 import recipeData from './recipes.json'
 
 
   
 function Groceries({itemlist, recipes}) {
-
   return (
     <Theme accentColor="gray">
       
@@ -52,7 +52,7 @@ function Groceries({itemlist, recipes}) {
           </Box>
 
           <Box grow="3">
-            <RecommendedRecipes recipe ={recipes} />
+            <RecommendedRecipes recipes ={recipes} />
           </Box>
           
         </ScrollArea>     
@@ -117,33 +117,60 @@ function ProductTable({itemlist}) {
 function AddRecipe(){
 }
 
-function RecommendedRecipes({recipes}){
+function RecipeCard ({recipe}) {
+  
+  return(
+    <>
+      <Dialog.Root>
+        <Dialog.Trigger className='recipe-button'>
+          <Box width="auto" height="30">
+            <Card size="2" style={{ maxWidth: 240 }}>              
+              <Inset clip="padding-box" side="top" pb="current">
+              <img
+              src="https://images.unsplash.com/photo-1617050318658-a9a3175e34cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=80"
+              alt="Bold typography"
+              style={{
+                display: 'block',
+                objectFit: 'cover',
+                width: '100%',
+                height: 140,
+                backgroundColor: 'var(--gray-5)',
+                }}
+              />
+              </Inset>
+              <Text as="p" size="3">
+              <Strong>{recipe.name}</Strong>
+              </Text>
+            </Card>
+          </Box>
+        </Dialog.Trigger>
+        <Dialog.Portal>
+            <Dialog.Overlay className='recipe-modal-overlay'>
+            <Dialog.Content className='recipe-modal' >
+                <h2>{recipe.name}</h2>
+            </Dialog.Content>
+            </Dialog.Overlay>
+          </Dialog.Portal>
+      </Dialog.Root>
+    </>
+      
+  )
+}
+
+function RecommendedRecipes({recipes}) {
+const recipesList = []
+
+recipes.forEach(rec => {
+  recipesList.push(rec);
+})
   return(
     <Theme>
       <Heading align="left">Recommended Recipes</Heading>
       
       <Flex gap="9">
-        <Box width="auto" height="30">
-          <Card size="2" style={{ maxWidth: 240 }}>
-          <Inset clip="padding-box" side="top" pb="current">
-          <img
-          src="https://images.unsplash.com/photo-1617050318658-a9a3175e34cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=80"
-          alt="Bold typography"
-          style={{
-            display: 'block',
-            objectFit: 'cover',
-            width: '100%',
-            height: 140,
-            backgroundColor: 'var(--gray-5)',
-            }}
-          />
-          </Inset>
-            <Text as="p" size="3">
-            <Strong>Recipe1</Strong>Randome recipes
-            </Text>
-          </Card>
-        </Box>
-        <Box width="auto" height="30">
+      <RecipeCard recipe={recipesList[0]}/>
+
+        {/* <Box width="auto" height="30">
           <Card size="2" style={{ maxWidth: 240 }}>
           <Inset clip="padding-box" side="top" pb="current">
           <img
@@ -182,7 +209,7 @@ function RecommendedRecipes({recipes}){
             <Strong>Recipe 3 Name</Strong>
             </Text>
           </Card>
-        </Box>
+        </Box> */}
       </Flex>
       <Button variant="outline">
       <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
