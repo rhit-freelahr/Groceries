14270b2f5d6e43b77af0e8431c93b95cea13614b
freelahr <freelahr@rose-hulman.edu>
2024-02-16 07:34:36 -0500

    more stuff

--- src/App.js
+++ src/App.js
@@ -456,40 +456,61 @@ function RecipeListTable({ recipes, setRecipes }) {
     setRecipeToDelete(null);
   };
 
-  const parseRecipe = (recipe) => {
-    const ingredientsList = recipe.ingredients.map((ingredient) => ingredient.itemName);
+  // const parseRecipe = (recipe) => {
+  //   const ingredientsList = recipe.ingredients.map((ingredient) => ingredient.itemName);
 
-    return (
-      <Table.Row key={recipe.id}>
-        <Table.Cell>
-          <Checkbox />
-        </Table.Cell>
-        <Table.Cell>{recipe.name}</Table.Cell>
-        <Table.Cell>{ingredientsList.join(', ')}</Table.Cell>
-        <Table.Cell>
-          <svg onClick={() => handleDeleteIconClick(recipe)}
-            style={{ cursor: 'pointer' }} width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 1C5.22386 1 5 1.22386 5 1.5C5 1.77614 5.22386 2 5.5 2H9.5C9.77614 2 10 1.77614 10 1.5C10 1.22386 9.77614 1 9.5 1H5.5ZM3 3.5C3 3.22386 3.22386 3 3.5 3H5H10H11.5C11.7761 3 12 3.22386 12 3.5C12 3.77614 11.7761 4 11.5 4H11V12C11 12.5523 10.5523 13 10 13H5C4.44772 13 4 12.5523 4 12V4L3.5 4C3.22386 4 3 3.77614 3 3.5ZM5 4H10V12H5V4Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
-        </Table.Cell>
-      </Table.Row>
-    );
-  };
+  //   return (
+  //     <Table.Row key={recipe.id}>
+  //       <Table.Cell>
+  //         <Checkbox />
+  //       </Table.Cell>
+  //       <Table.Cell>{recipe.name}</Table.Cell>
+  //       <Table.Cell>{ingredientsList.join(', ')}</Table.Cell>
+  //       <Table.Cell>
+  //         <svg onClick={() => handleDeleteIconClick(recipe)}
+  //           style={{ cursor: 'pointer' }} width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 1C5.22386 1 5 1.22386 5 1.5C5 1.77614 5.22386 2 5.5 2H9.5C9.77614 2 10 1.77614 10 1.5C10 1.22386 9.77614 1 9.5 1H5.5ZM3 3.5C3 3.22386 3.22386 3 3.5 3H5H10H11.5C11.7761 3 12 3.22386 12 3.5C12 3.77614 11.7761 4 11.5 4H11V12C11 12.5523 10.5523 13 10 13H5C4.44772 13 4 12.5523 4 12V4L3.5 4C3.22386 4 3 3.77614 3 3.5ZM5 4H10V12H5V4Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+  //       </Table.Cell>
+  //     </Table.Row>
+  //   );
+  // };
+
+  const recipeMap = recipes => {
+    const mapped = recipes.map((recipe, index) => {
+      return <RecipeCard recipe={recipe} canDelete={true}/>
+    })
+    if(recipes.length % 3 == 0) {
+      return mapped
+    } else if(recipes.length %3 == 1) {
+      return (
+        <>
+        {mapped}
+        {blankCard()}
+        {blankCard()}
+        </>
+      )
+    } else {
+      return (
+        <>
+        {mapped}
+        {blankCard()}
+        </>
+      )
+    }
+  }
 
   return (
     <Theme>
       <Text size="6" weight="bold" align = "center">
         Recipe List
       </Text>
-      <Table.Root>
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeaderCell>Do you want this?</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Recipe Name</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Ingredient</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>{recipes.map((recipe) => parseRecipe(recipe))}</Table.Body>
-      </Table.Root>
+      <Theme>
+      <div style={{textAlign:"center", maxWidth: "800px", margin: "0 auto"}}>
+        <Heading align="center">Recommended Recipes</Heading>
+            <Flex style={{justifyContent: "center", flexWrap: "wrap"}} gap="4">
+                {recipeMap(recipes)}
+              </Flex>
+      </div>
+    </Theme>
 
       {/* Delete Confirmation Dialog */}
       <Dialog.Root open={recipeToDelete !== null} onClose={handleDeleteCancel}>
@@ -646,7 +667,7 @@ function RecommendedRecipes({ recipes }) {
 
   const recipeMap = recipes => {
     const mapped = recipes.map((recipe, index) => {
-      return <RecipeCard recipe={recipe} canDelete={true}/>
+      return <RecipeCard recipe={recipe} canDelete={false}/>
     })
     if(recipes.length % 3 == 0) {
       return mapped
